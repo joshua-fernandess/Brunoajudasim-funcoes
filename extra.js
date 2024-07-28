231d9fa722231accdd8293431763588582c27e39
brunoscheifer <brunoscheifer11@gmail.com>
2024-07-28 15:08:13 -0300

    terminei o extra

--- extra.js
+++ extra.js
@@ -15,6 +15,16 @@ Programa desenvolvido por:
  - Aluno 4
 */
 
+function intersection(vetor1, vetor2) {
+    let array1 = new Set(vetor1)
+
+    let array2 = vetor2.filter(numero => array1.has(numero))
+
+        return array2
+}
+
+console.log(intersection([1, 2, 3], [3, 4, 5]))
+
 /*
 2. Crie uma função chamada drawEvenNumberTriangle que aceite um número n, use a função
 getEvenNumbers para gerar os primeiros n números pares e, em seguida, use esses números
@@ -24,3 +34,31 @@ Dica: Primeiro, crie a função getEvenNumbers que retorna os primeiros n númer
 Use um laço para iterar desde 1 até n e, em cada iteração, imprima os primeiros i
 números pares. Utilize a função getEvenNumbers para obter os números pares.
 */
+
+function getEvenNumbers(n) {
+    let pares = []
+    
+    for(let i = 0 ; i < n; i++) {
+        pares.push(i * 2)
+    }
+    return pares
+}
+
+function drawEvenNumberTriangle(n) {
+    let pares = getEvenNumbers(n *(n + 1 ) / 2)
+
+    let index = 0
+
+    for(let i = 1; i <= n; i++) {
+        let linha = ''
+        for(let l = 0; l < i; l++) {
+            if(index < pares.length) {
+            linha += pares[index] + ' '
+            index++
+            }
+        }
+        console.log(linha.trim())
+    }
+}
+
+console.log(drawEvenNumberTriangle(5))
